6ea35ab10e8ed03c801a53c3a275cd46ee4c35aa
SARNM <dalireza1374@gmail.com>
2019-05-20 13:23:25 +0430

    complete profile fragment

--- app/src/main/java/com/example/ms/piratilapp/Gamer/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/ms/piratilapp/Gamer/Fragment/ProfileFragment.java
@@ -7,18 +7,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ms.piratilapp.R;
 
 public class ProfileFragment extends Fragment {
+
+    Button btn_exit, btn_transactions, btn_checkout, btn_roles;
+    TextView txt_dimond_count;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
 
+        btn_exit=(Button)view.findViewById(R.id.btn_exit);
+        btn_transactions=(Button)view.findViewById(R.id.btn_transactions);
+        btn_checkout=(Button)view.findViewById(R.id.btn_checkout);
+        btn_roles=(Button)view.findViewById(R.id.btn_roles);
+        txt_dimond_count=(TextView) view.findViewById(R.id.txt_dimond_count);
+
 
 
         return view;
     }
+
 }
